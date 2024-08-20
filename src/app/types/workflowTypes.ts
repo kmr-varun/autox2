63caea8061d84faa2c0c1fd7c2d8564b1d02ac99
Varun Kumar <kmr11varun@gmail.com>
2024-08-20 09:57:54 +0530

    first commit

---
+++ src/app/types/workflowTypes.ts
@@ -0,0 +1,42 @@
+export interface Condition {
+    columnId: string;
+    columnName: string;
+    columnType: string;
+    fromValue: string;
+    toValue: string;
+    operator: string;
+    operatorString: string;
+    multi: boolean;
+    conditionType: string;
+  }
+  
+  export interface ActionData {
+    name: string;
+    type: string;
+    value: any[];
+  }
+  
+  export interface Actions {
+    id: string;
+    name: string;
+    operation: string;
+    setup: boolean;
+    data: ActionData[];
+  }
+  
+  export interface WorkflowState {
+    id: string;
+    name: string;
+    workflowName: string;
+    workflowDesc: string;
+    workflowType: string;
+    workflowData: [];
+    pipelineId: string;
+    pipelineName: string;
+    orgId: string;
+    createdBy: string;
+    updatedBy: string;
+    conditions: Condition[];
+    actions: Actions[];
+  }
+  
